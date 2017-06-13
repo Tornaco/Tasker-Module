22d89bac72f44016cdc6b802a4a40f5858f4b742
NickGuo <guohao4@lenovo.com>
2017-06-13 15:06:25 +0800

    Add more elements

--- app/src/main/java/dev/tornaco/tasker/module/ExampleModule.java
+++ app/src/main/java/dev/tornaco/tasker/module/ExampleModule.java
@@ -1,12 +1,21 @@
 package dev.tornaco.tasker.module;
 
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.UiDevice;
 import android.widget.Toast;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 import static java.lang.Thread.sleep;
 
@@ -14,7 +23,28 @@ import static java.lang.Thread.sleep;
  * Created by Nick on 2017/6/7 14:23
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleModule {
+
+    @Before
+    public void setup() {
+        MainThreadHandler.getMainThreadHandler().post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(InstrumentationRegistry.getTargetContext(), "@Before", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @After
+    public void teardown() {
+        MainThreadHandler.getMainThreadHandler().post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(InstrumentationRegistry.getTargetContext(), "@After", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
 
     @Test
     public void pressHome() {
@@ -60,5 +90,26 @@ public class ExampleModule {
 
         // Wait for dismiss.
         sleep(3 * 1000);
+    }
+
+    @Test
+    public void launch3rdApp() {
+        // Launch 3-rd app.
+        PackageManager pm = InstrumentationRegistry.getTargetContext().getPackageManager();
+        List<PackageInfo> packageInfos;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            packageInfos = pm.getInstalledPackages(PackageManager.MATCH_UNINSTALLED_PACKAGES);
+        } else {
+            packageInfos = pm.getInstalledPackages(PackageManager.GET_UNINSTALLED_PACKAGES);
+        }
+        for (PackageInfo packageInfo : packageInfos) {
+            if ((packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 1) {
+                continue;
+            }
+
+            Intent intent = pm.getLaunchIntentForPackage(packageInfo.packageName);
+            if (intent == null) continue;
+            InstrumentationRegistry.getTargetContext().startActivity(intent);
+        }
     }
 }
